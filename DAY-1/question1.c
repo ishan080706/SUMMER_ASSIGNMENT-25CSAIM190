ae45e671f9f625ce896b1b5de2b6870cfc0cc4a7
ishan080706 <ishan080706@gmail.com>
2026-06-09 20:33:43 +0530

    Add files via upload

---
+++ DAY-1/question1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int N;
+    long long sum;
+    
+    printf("Enter number:");
+    scanf("%d", &N);
+
+    sum = (long long)N * (N + 1) / 2;
+    printf("Sum of first %d natural numbers is = %lld\n",N,sum);
+
+        return 0;
+}
