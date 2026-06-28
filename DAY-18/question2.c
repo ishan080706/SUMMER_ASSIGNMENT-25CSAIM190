13b53e9ebbe625219364174480924a9a4073271d
ishan080706 <ishan080706@gmail.com>
2026-06-28 12:32:23 +0530

    commit

---
+++ DAY-18/question2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, minIndex, temp;
+    int arr[100];
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n - 1; i++)
+    {
+        minIndex = i;
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i)
+        {
+            temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    printf("Sorted array in ascending order:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
